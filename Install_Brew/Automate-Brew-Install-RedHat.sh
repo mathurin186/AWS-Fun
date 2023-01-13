1a6ad0603df771cfefb2132186e3630566742dc3
Mathurin Kasten <xmathurinx@gmail.com>
2023-01-12 18:42:36 -0800

    Updated with removed peices of code

--- Install_Brew/Automate-Brew-Install-RedHat.sh
+++ Install_Brew/Automate-Brew-Install-RedHat.sh
@@ -1,14 +1,8 @@
 #/bin/bash/
 
 """
-Fresh install of brew, git, and ruby in a CentOS instance. 
+Fresh install of brew in a CentOS instance. 
 """
-
-# Install Git
-sudo yum install git -y
-
-# Install Ruby
-sudo yum install ruby -y
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
